d0e425f2670fab878b266e6796247313b98a1e57
mishalp <mishalclever@gmail.com>
2023-08-04 15:02:37 +0530

    slides added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './navbar.css'
 import PrimaryBtn from '../primaryBtn/PrimaryBtn'
+import { StoreContext } from '../../store/StoreContext'
 
 function Navbar() {
+
+  const { model, models, slides } = useContext(StoreContext)
+
+  const move = (index) => {
+    slides.current.splide.Components.Move.move(
+      index,
+      index,
+      index+1
+    )
+  }
+
   return (
     <div className='navbar' id='navbar'>
-        <div className="left">
-            <p>FLEXI</p>
-        </div>
-        <div className="center">
-            <PrimaryBtn content='Model E' theme='white' />
-            <PrimaryBtn content='Model C' theme='white' />
-            <PrimaryBtn content='Model O' theme='white' />
-        </div>
-        <div className="right">
-            <PrimaryBtn content='Login' theme='black' />
-        </div>
+      <div className="left">
+        <p>FLEXI</p>
+      </div>
+      <div className="center">
+        {models.map((item, index) => (
+          <PrimaryBtn content={item.name} handleClick={() => move(index)} theme={model === index ? 'black' : 'white'} />
+        ))}
+      </div>
+      <div className="right">
+        <PrimaryBtn content='Login' theme='black' />
+      </div>
     </div>
   )
 }
